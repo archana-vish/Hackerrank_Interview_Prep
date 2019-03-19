effa208a3e5e69603c9af33e41710f89c1ee83f1
Archana Viseswaran <A.Visveswaran@greenwich.ac.uk>
2019-03-19 10:49:56 +0000

    Initial files

---
+++ src/main/java/CloudJump.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class CloudJump {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c) {
+
+        int tot = 0, index = 0;
+
+        if (c.length == 0) {
+            return 0;
+        }
+
+        Map<Integer, Integer> mapOfClouds = new HashMap<>();
+        for (int n : c) {
+            mapOfClouds.put(index++, n);
+        }
+
+        System.out.println(mapOfClouds.size());
+
+        for (int key = 0; key < mapOfClouds.size() - 1;) {
+
+            System.out.println(key + "..." + tot);
+            if (((key + 2) < mapOfClouds.size()) && mapOfClouds.get(key + 2) == 0) {
+                key += 2;
+            } else {
+                key++;
+            }
+
+            tot += 1;
+        }
+
+        System.out.println(tot);
+        return tot;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int n = 7;
+
+        int[] c = new int[]{0, 0, 1, 0, 0, 1, 0};
+
+        int result = jumpingOnClouds(c);
+
+        scanner.close();
+    }
+}
---
+++ src/main/java/RepeatSubstring.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class RepeatSubstring {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+
+        if (n == 0) {
+            return -1L;
+        } else if (n == 1) {
+            return 1L;
+        } else if (s.length() == 1 && s.equals("a")) {
+            return n;
+        }
+        long frequencyOfAinString = Arrays.stream(s.split("")).filter(l -> l.equals("a")).count();
+
+
+        long fineSplit = n/s.length();
+        long finite = (frequencyOfAinString * fineSplit);
+        System.out.println("finite.." + finite);
+
+
+        long remainder = n%s.length();
+        System.out.println(remainder);
+        String sub = s.substring(0, (int) remainder);
+        long f2 = Arrays.stream(sub.split("")).filter(l -> l.equals("a")).count();
+        System.out.println(f2);
+
+        return finite + f2;
+
+
+//        int originalLength = s.length();
+//
+//        frequency = (long) Math.ceil((1f * n * frequencyOfAinString )/ originalLength);
+//
+//        return frequency;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+         String s = "gfcaaaecbg";
+
+        long n = 547602L;
+
+        //long n = 12;
+
+//        String s = "aab";
+//
+//        long n = 11;
+
+        long result = repeatedString(s, n);
+
+        System.out.println(result);
+
+    }
+}
